16a38f65686571a1eaaf75ef3707ce03700b803f
jean claude Misigaro <mjeanclaude23@gmail.com>
2023-03-27 10:46:35 +0200

    Add project files.

---
+++ GmailProject/SendEmailPage.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendEmailPage.aspx.cs" Inherits="GmailProject.SendEmailPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Sending Email</h1>
+            <table>
+                <tr>
+                    <td>To:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" cols="30" rows="4" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Subject:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Message:</td>
+                    <td>
+                        <textarea id="TextArea1" cols="30" rows="4" runat="server"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="sendmail" runat="server" Text="Send" OnClick="sendmail_Click" style="color: blue" />
+                    </td>
+                </tr>
+            </table>
+            </div>
+
+    </form>
+</body>
+</html>
---
+++ GmailProject/SendEmailPage.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+
+namespace GmailProject
+{
+    public partial class SendEmailPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void sendmail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //calling the Path of the json file configured from Google cloud
+                string[] Scopes = { GmailService.Scope.GmailSend };
+                UserCredential credential;
+                using (var stream = new FileStream(
+                    "C:/Users/CTRL-SHIFT Ltd/source/repos/GmailProject/GmailProject/credentials.json",
+                    FileMode.Open,
+                    FileAccess.Read
+                ))
+                {
+                    string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                    credPath = Path.Combine(credPath, ".credentials/", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                                 GoogleClientSecrets.FromStream(stream).Secrets,
+                                  Scopes,
+                                  "user",
+                                  CancellationToken.None,
+                                  new FileDataStore(credPath, true)).Result;
+                }
+                // Creation of the Gmail API service.
+                var service = new GmailService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "GmailProject",
+                });
+                //describing the syntactic roles of the HTML components
+                string message = $"To: {TextBox1.Text}\r\nSubject: {TextBox2.Text}\r\nContent-Type: text/html;charset=utf-8\r\n\r\n{TextArea1.InnerText}";
+                var newMsg = new Google.Apis.Gmail.v1.Data.Message();
+                newMsg.Raw = this.Base64UrlEncode(message.ToString());
+                Message response = service.Users.Messages.Send(newMsg, "me").Execute();
+                if (response != null)
+                {
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Email Successfully Sent!!')", true);
+                    TextBox1.Text = TextBox2.Text = TextArea1.InnerText = "";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private string Base64UrlEncode(string input)
+        {
+            //Method of refreshing the submitted data to free text fields and text areas.
+            try
+            {
+                var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                return Convert.ToBase64String(inputBytes)
+                  .Replace('+', '-')
+                  .Replace('/', '_')
+                  .Replace("=", "");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
